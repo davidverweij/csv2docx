4f53c9f0d8372d599647b37c4bfe2ef2897ec8c4
Jay Rainey <jawrainey@gmail.com>
2020-04-30 20:30:31 +0100

    Move code to one method
    - Removed all error handling issues as they will be resolved when the codebase is moved to click (CLI);
    - Made use of Python's f strings;
    - Added several notes of where/why I made changes (please remove on PR);
    - Used set difference instead of for loop for simplicity;

--- docx-csv-mailmerge.py
+++ docx-csv-mailmerge.py
@@ -1,58 +1,40 @@
-#!/usr/bin/env python
-
-__author__ = "David Verweij"
-__license__ = "MIT"
-__version__ = "0.1"
-__email__ = "davidverweij@gmail.com"
-__status__ = "Work in Progress"
-
 from mailmerge import MailMerge
-import sys
 import csv
+import sys
 
-if __name__ == '__main__':                      #code to execute if called from command-line
-    try:
-        print ("Getting .docx template and .csv data files ...")
-        csvfile = open(sys.argv[1], 'rt')
-        docx = MailMerge(sys.argv[2])
-        delimiter = sys.argv[3]
-        csvreader = csv.reader(csvfile, delimiter=delimiter)
+def convert(data, template, delimiter):
+    print ("Getting .docx template and .csv data files ...")
 
-    except IndexError:
-        print ("Error: Insufficient arguments")
-    except IOError:
-        print ("Error: Files not found")
-    except:
-        print ("Error: Unexpected")
-        raise
-    else:
+    with open(data, 'rt') as csvfile:
+        csv_opened = csv.reader(csvfile, delimiter=delimiter)
+        csv_headers = next(csv_opened)
+
+        # ASSIGNING HERE TO: (i) OUTPUT TO USER; (ii) VALIDATE INPUT.
+        docx = MailMerge(template)
         docx_mergefields = docx.get_merge_fields()
-        csv_headers = next(csvreader)
-        print ("Found these merge fields in the .docx : ", docx_mergefields)
-        print ("Found these header fields in the .csv : ", csv_headers)
 
-        for field in docx_mergefields:                      # see if all fields are accounted for in the .csv header
-            if field not in csv_headers:
-                print("I can't find the mergefield " +
-                      field + " in the .csv file. Please" +
-                      "check again and ensure the headers " +
-                      "are exactly as the mergfields names. Case sensitive.")
-                break
-        else:                           # if all fields are accounted for
-            print("All merge fields are present in the .csv headers. Generating Word files now...")
-            csvdict = csv.DictReader(open(sys.argv[1], 'rt'), delimiter=delimiter)                        # have to reopen, because the 'next'
+        print(f"DOCX fields : {docx_mergefields}")
+        print(f"CSV field   : {csv_headers}")
 
-            for counter, row in enumerate(csvdict):
-                single_document = {key : row[key] for key in docx_mergefields}       # dictionary comprehension to get mergefields from csv
-                docx = MailMerge(sys.argv[2])                                        # get the template
-                try:
-                    docx.merge_templates([single_document], separator='page_break')                              # passing it as a 'list' (of one) to allow prepoluated objects as data
-                except ValueError:
-                    print ("ValueError in document number " + str(counter) + ". Please check the .csv for valid characters. Continuing with the rest...")
-                except:
-                    print("Uknown Error")
-                    raise
-                else:
-                    docx.write(str(counter) + ".docx")                               # TODO: haven't found a way to write to a subfolder
+        # see if all fields are accounted for in the .csv header
+        column_in_data = set(docx_mergefields) - set(csv_headers)
+        if len(column_in_data) > 0:
+            print (f"{column_in_data} is in the word document, but not csv.")
+            return
 
-    print("Finished Program.")
+    print("All merge fields are present in the .csv headers. Generating Word files now...")
+    # have to reopen, because the 'next'
+    with open(data, 'rt') as csvfile:
+        csvdict = csv.DictReader(csvfile, delimiter=delimiter)
+        for counter, row in enumerate(csvdict):
+            single_document = {key : row[key] for key in docx_mergefields}
+            docx.merge_templates([single_document], separator='page_break')
+            # TODO: haven't found a way to write to a subfolder
+            docx.write("{counter}.docx")
+
+if __name__ == '__main__':
+    # TODO: validate user input independent of the convert method
+    # Showing how unpacking data works
+    _, data, template, delimiter = sys.argv
+    # Invoke our main function above (ideally via a CLI like click)
+    convert(data, template, delimiter)
