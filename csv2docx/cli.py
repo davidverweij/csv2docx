cb3c373a4d0a783640c6ff8a675dc9be76df549e
David Verweij <davidverweij@gmail.com>
2020-05-04 17:36:57 +0100

    forgot to fix merge conflicts at cli.py

--- csv2docx/cli.py
+++ csv2docx/cli.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import click
-import csv2docx.csv2docx as c2d
-=======
 import csv2docx.csv2docx as c2d
 import click
->>>>>>> c51178f44317b3f6a403b76c4a6b31aa76198e6f
+
 
 
 @click.command()
@@ -19,11 +15,6 @@ import click
 @click.option(
     '--delimiter', '-d',
     default=";",
-<<<<<<< HEAD
-    help='delimiter used in your csv. Default is \';\'')
-def main(data, template, delimiter):
-	c2d.convert(data, template, delimiter)
-=======
     help='Delimiter used in your csv. Default is \';\'')
 @click.option(
     '--destination', '-dest',
@@ -31,4 +22,3 @@ def main(data, template, delimiter):
     help='The destination to store the files.')
 def main(data, template, delimiter, destination):
     c2d.convert(data, template, delimiter, destination)
->>>>>>> c51178f44317b3f6a403b76c4a6b31aa76198e6f
