35746857579ed46288a59b47d2aa6a20d23a4f2a
David Verweij <davidverweij@gmail.com>
2020-05-07 18:11:16 +0100

    added linting using flake8

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,15 @@
 import nox
 
+locations = "csv2docx", "tests", "noxfile.py"
+
 @nox.session(python=["3.8"])
 def tests(session):
     """Run the test suite using poetry"""
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
+
+@nox.session(python=["3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
