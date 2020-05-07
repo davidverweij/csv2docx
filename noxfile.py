5ba190bd38ceb2cc320909ca9c2fe67a669d504e
David Verweij <davidverweij@gmail.com>
2020-05-07 18:44:41 +0100

    pinned dev and test dependencies through poetry.lock

--- noxfile.py
+++ noxfile.py
@@ -1,25 +1,71 @@
+# standard libraries
+import tempfile
+
+# third-party packages
 import nox
 
-nox.options.sessions = "lint", "tests"
+# local packages
+# -- none
+
+
+nox.options.sessions = "lint", "safety", "tests"
 locations = "csv2docx", "tests", "noxfile.py"
+
+
+# a wrapper for session.install to pin dependecies in testing
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.8"])
 def tests(session):
-    """Run the test suite using poetry"""
-    session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
+    session.run("pytest", *args)
 
 
 @nox.session(python=["3.8"])
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python=["3.8"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
     session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
